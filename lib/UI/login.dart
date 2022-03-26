6c5c7810476f71ad2599ed26527ebd6dfd19f8ea
lennie-ein <lenniemaina05@gmail.com>
2022-03-26 21:33:06 +0300

    login page added

--- lib/UI/login.dart
+++ lib/UI/login.dart
@@ -1,4 +1,7 @@
+import 'package:bar_manager/Utils/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ms_undraw/ms_undraw.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +13,43 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: HypeColors.HypeBgBlue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: UnDraw(
+              illustration: UnDrawIllustration.login,
+              color: HypeColors.HypeBlue,
+              height: 200,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(CupertinoIcons.pencil_ellipsis_rectangle),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)))),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(CupertinoIcons.pencil_ellipsis_rectangle),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)))),
+            ),
+          ),
+          TextButton(onPressed: () {}, child: Text("Login"), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(HypeColors.HypeLightBronze)),)
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+mixin HypeColors{
+  static Color HypeBlue = Color(0xFF0c5cb2);
+  static Color HypeLightBlue = Color(0xFF69ACF5);
+  static Color HypeBronze = Color(0xFFB0980C);
+  static Color HypeLightBronze = Color(0xFFf4d35e);
+  static Color HypeBgBlue = Color(0xFFd9ead3);
+}
