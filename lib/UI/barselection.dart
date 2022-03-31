969e5cc1052765f45b383664863e122237e0b7a6
lennie-ein <lenniemaina05@gmail.com>
2022-03-31 21:19:15 +0300

    Adding a page to select the different shops

---
+++ lib/UI/barselection.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BarSelectionScreen extends StatefulWidget {
+  const BarSelectionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BarSelectionScreen> createState() => _BarSelectionScreenState();
+}
+
+class _BarSelectionScreenState extends State<BarSelectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
